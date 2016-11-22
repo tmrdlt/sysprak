5e249752823119fc81f04446dd0309d8f373eaa5
Timo Erdelt <erdelt@cip.ifi.lmu.de>
2016-11-22 16:59:18 +0100

    main v0.0.1: read GameID from flag

--- main.c
+++ main.c
@@ -1,7 +1,42 @@
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello World\n");
-    //testing
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-    return 0;
+
+void printHelp() {
+        printf("How to use:\n");
+        printf("Please add -g <gameid>\n");
+}
+int main(int argc, char *argv[]) {
+
+        int gameid = 0;
+
+        // read gameid with -g flag
+        int ret;
+        while ((ret = getopt(argc, argv, "g:")) != -1) {
+            switch (ret) {
+                case 'g':
+                        gameid = atoi(optarg);
+                        break;
+                default:
+                        printHelp();
+                        return EXIT_FAILURE;
+                        break;
+                }
+        }
+        if (gameid == 0) {
+                printHelp();
+                return EXIT_FAILURE;
+        }
+
+
+
+
+        printf("GameID: %d\n", gameid);
+
+        // insert code here...
+        printf("...for testing purposes\n");
+        //testing
+
+        return EXIT_SUCCESS;
 }
