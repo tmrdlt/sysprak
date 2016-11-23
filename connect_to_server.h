f99e3b187003e5ac925dab0dc19217568e3bca70
Holzner <Stephan@dhcp-10-181-29-50.dynamic.eduroam.mwn.de>
2016-11-23 13:48:44 +0100

    connect ..h Upload

---
+++ connect_to_server.h
@@ -0,0 +1,14 @@
+//
+//  connect_to_server.h
+//  bashni
+//
+//  Created by Holzner on 23.11.16.
+//  Copyright © 2016 System-Praktikum. All rights reserved.
+//
+
+#ifndef connect_to_server_h
+#define connect_to_server_h
+
+int connect_to_server ();
+
+#endif /* connect_to_server_h */
